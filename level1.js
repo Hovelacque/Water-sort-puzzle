1fa139c7f8d7c215166bc566726441de5dbd17b8
Gustavo <gustavo@kapibara.com.br>
2024-01-21 20:52:24 -0300

    Ajustando Tubos e posição das Cores

--- level1.js
+++ level1.js
@@ -4,48 +4,48 @@ const initialColors = [
         "red",
         "red",
         "red",
-        "red",
-        "red"
+        // "red",
+        // "red"
     ],
     [
         "blue",
         "blue",
         "blue",
         "blue",
-        "blue",
-        "blue"
+        // "blue",
+        // "blue"
     ],
     [
         "green",
         "green",
         "green",
         "green",
-        "green",
-        "green"
+        // "green",
+        // "green"
     ],
     [
         "yellow",
         "yellow",
         "yellow",
         "yellow",
-        "yellow",
-        "yellow"
+        // "yellow",
+        // "yellow"
     ],
     [
         "orange",
         "orange",
         "orange",
         "orange",
-        "orange",
-        "orange"
+        // "orange",
+        // "orange"
     ],
     [
         "purple",
         "purple",
         "purple",
         "purple",
-        "purple",
-        "purple"
+        // "purple",
+        // "purple"
     ],
     [
     ]
